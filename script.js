e9a1d35001e031fcc8f543ad0fc541e68d919f7e
Gustavo Rocha <97960688+gu-rocha@users.noreply.github.com>
2022-07-06 08:10:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-var seconds = 00; 
 var tens = 00; 
+var seconds = 00;
+var minutes = 00; 
 var appendTens = document.getElementById("tens")
 var appendSeconds = document.getElementById("seconds")
+var appendMinutes = document.getElementById("minutes")
 var buttonStart = document.getElementById('button-start');
 var buttonStop = document.getElementById('button-stop');
 var buttonReset = document.getElementById('button-reset');
 var Interval;
 
-    
+
 buttonStart.onclick = function() {
-  
   clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);
+  Interval = setInterval(startTimer, 10);
 }
 
-  buttonStop.onclick = function() {
-      clearInterval(Interval);
+buttonStop.onclick = function() {
+  clearInterval(Interval);
 }
-
 
 buttonReset.onclick = function() {
-    clearInterval(Interval);
+  clearInterval(Interval);
   tens = "00";
-    seconds = "00";
+  seconds = "00";
+  minutes = "00";
   appendTens.innerHTML = tens;
-    appendSeconds.innerHTML = seconds;
+  appendSeconds.innerHTML = seconds;
+  appendMinutes.innerHTML = minutes;
 }
-    
+
 
 function startTimer () {
   tens++; 
@@ -48,10 +50,20 @@ function startTimer () {
     appendTens.innerHTML = "0" + 0;
   }
   
-  if (seconds > 9){
+  if (seconds > 9) {
     appendSeconds.innerHTML = seconds;
   }
 
+  if (seconds > 59) {
+    console.log("minutes");
+    minutes++;
+    appendMinutes.innerHTML = "0" + minutes;
+    seconds = 0;
+    appendSeconds.innerHTML = "0" + 0;
+  }
+  
+  if (minutes > 9) {
+    appendMinutes.innerHTML = minutes;
+  }
+  
 }
-    
- 
